b3136e59e2a984ff8e966cc31369ba84d6b3078c
Bablu <bablu.chilukoti@gmail.com>
2023-10-26 19:22:14 +0530

    Automation

--- Automation.sh
+++ Automation.sh
@@ -1,9 +1,9 @@
 # shellcheck disable=SC1036
 copy_function() {
-  echo -e "\e[34m >>>>>>>>>>>>>>>>>>changing the directory>>>>>>>>>>>>>>>>>>>>>>>>[0m"
-  cd Learn-YAML
-  echo -e "\e[34m >>>>>>>>>>>>>>>>>>pulling the git>>>>>>>>>>>>>>>>>>>>>>>[0m"
-  git pull >/dev/null
+  #echo -e "\e[34m >>>>>>>>>>>>>>>>>>changing the directory>>>>>>>>>>>>>>>>>>>>>>>>[0m"
+  #cd Learn-YAML
+  #echo -e "\e[34m >>>>>>>>>>>>>>>>>>pulling the git>>>>>>>>>>>>>>>>>>>>>>>[0m"
+  #git pull >/dev/null
   echo -e "\e[34m >>>>>>>>>>>>>>>>>>copying the file>>>>>>>>>>>>>>>>>>>>>>>[0m"
   cp ${varibale} /etc/ansible/playbooks
   cd /etc/ansible/playbooks
